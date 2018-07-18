a2f61d752339886f99961883174fc74932010d2b
Damian Baćkowski <damianb@profill.com.pl>
2018-07-18 08:01:17 +0200

    fix bugs

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/api/forecast', async (req, res) => {
-  const geoApiUri = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(escape(dotEnvConfig.parsed['CITY']))}&key=${dotEnvConfig.parsed['GOOGLE_API_KEY']}`;
+  const geoApiUri = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(dotEnvConfig.parsed['CITY'])}&key=${dotEnvConfig.parsed['GOOGLE_API_KEY']}`;
   const geocode = await request({ uri: geoApiUri, json: true });
   const { lat, lng } = geocode['results'][0]['geometry']['location'];
 
